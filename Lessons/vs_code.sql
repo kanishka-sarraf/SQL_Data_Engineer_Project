0a63c6d3d9c530a77d5792702f8a1c51441b424f
kanishka-sarraf <kanishkasarraf20@gmail.com>
2026-02-17 15:59:26 +0530

    Updated the file

--- Lessons/vs_code.sql
+++ Lessons/vs_code.sql
@@ -1,1 +1,25 @@
 SELECT 42 as answer;
+
+SELECT *
+FROM information_schema.tables;
+
+SELECT *
+FROM information_schema.tables
+WHERE table_catalog = 'data_jobs';
+
+select * from job_postings_fact limit 10;
+
+explain analyse
+select 
+    cd.name as company_name,
+    count(jpf.job_id) as posting_count
+from job_postings_fact jpf
+left join company_dim cd
+    on jpf.company_id = cd.company_id
+where jpf.job_country = 'United States'
+group by cd.name
+having count(jpf.job_id) > 3000
+order by posting_count desc
+limit 10;
+
+select job_title_short from job_postings_fact;
